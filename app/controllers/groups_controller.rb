fa8d5da81f63d35107951708191ff80e504d1ad1
linjiaxin <linjiaxin405@gmail.com>
2016-12-15 20:02:34 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,6 +30,13 @@ class GroupsController < ApplicationController
 		redirect_to groups_path, notice: "保存成功"
 	end
 
+	def destroy
+		@group = Group.find(params[:id])
+		@group.destroy
+		flash[:alert] = "删除成功"
+		redirect_to groups_path
+	end
+
 	private
 
 	def group_params
